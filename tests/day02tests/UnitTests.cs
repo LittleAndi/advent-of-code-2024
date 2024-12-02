e487178d055ec2d91947b01b3ef4f199c2056af8
Lars Nyström <nystrom.lasse@gmail.com>
2024-12-03 00:31:24 +0100

    Day02 (#3)
    * Add day02
    
    * Solution for day 2

---
+++ tests/day02tests/UnitTests.cs
@@ -0,0 +1,34 @@
+﻿using Shouldly;
+
+namespace day02tests;
+
+public class UnitTest1
+{
+    [Theory]
+    [InlineData(new int[] { 7, 6, 4, 2, 1 }, true)]
+    [InlineData(new int[] { 1, 2, 7, 8, 9 }, false)]
+    [InlineData(new int[] { 9, 7, 6, 2, 1 }, false)]
+    [InlineData(new int[] { 1, 3, 2, 4, 5 }, false)]
+    [InlineData(new int[] { 8, 6, 4, 4, 1 }, false)]
+    [InlineData(new int[] { 1, 3, 6, 7, 9 }, true)]
+    [InlineData(new int[] { 43, 46, 47, 48, 50, 52, 56 }, false)]
+    public void ShouldReturnSafeOrNot(int[] report, bool shouldBeSafe)
+    {
+        var analyzer = new ReportAnalyzer(report);
+        analyzer.IsSafe.ShouldBe(shouldBeSafe);
+    }
+
+    [Theory]
+    [InlineData(new int[] { 7, 6, 4, 2, 1 }, true)]
+    [InlineData(new int[] { 1, 2, 7, 8, 9 }, false)]
+    [InlineData(new int[] { 9, 7, 6, 2, 1 }, false)]
+    [InlineData(new int[] { 1, 3, 2, 4, 5 }, true)]
+    [InlineData(new int[] { 8, 6, 4, 4, 1 }, true)]
+    [InlineData(new int[] { 1, 3, 6, 7, 9 }, true)]
+    public void ShouldReturnSafeOrNotUsingProblemDampener(int[] report, bool shouldBeSafe)
+    {
+        var analyzer = new ReportAnalyzer(report);
+        analyzer.IsSafeWithProblemDampener.ShouldBe(shouldBeSafe);
+    }
+
+}
