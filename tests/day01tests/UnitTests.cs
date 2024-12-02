9da3e2a1e7236c7699218ca2d1e23d8183ce08f2
Lars Nyström <nystrom.lasse@gmail.com>
2024-12-02 21:57:10 +0100

    Solution for day 1

---
+++ tests/day01tests/UnitTests.cs
@@ -0,0 +1,26 @@
+﻿namespace day01tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void ShouldReturnZeroDistance()
+    {
+        var dc = new DistanceCalculator([], []);
+        dc.Distance.ShouldBe(0);
+    }
+
+    [Fact]
+    public void ShouldReturnDistance()
+    {
+        var dc = new DistanceCalculator([3, 4, 2, 1, 3, 3], [4, 3, 5, 3, 9, 3]);
+        dc.Distance.ShouldBe(11);
+    }
+
+    [Fact]
+    public void ShouldReturnSimilarityScore()
+    {
+        var dc = new DistanceCalculator([3, 4, 2, 1, 3, 3], [4, 3, 5, 3, 9, 3]);
+        dc.SimilarityScore.ShouldBe(31);
+    }
+
+}
