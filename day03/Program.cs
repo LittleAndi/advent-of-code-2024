c3e09b71719df1391ebea769f396cd51e1655c8f
Lars Nyström <lars.nystrom@mio.se>
2024-12-03 12:39:59 +0100

    Solution for part 1 of day 3

--- day03/Program.cs
+++ day03/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+var line = File.ReadAllText("input.txt");
+
+var computer = new Computer(line);
+var sum = computer.CalculateSumOfUncorruptedMemory();
+Console.WriteLine($"Sum of multiplications: {sum}");
+
+public class Computer(string corruptedMemory)
+{
+    private readonly string corruptedMemory = corruptedMemory;
+    private readonly Regex regex = new(@"(mul\(\d{1,3},\d{1,3}\))");
+    private readonly Regex mulRegex = new(@"mul\((\d{1,3}),(\d{1,3})\)");
+
+    public int CalculateSumOfUncorruptedMemory()
+    {
+        var sum = 0;
+        var matches = regex.Matches(corruptedMemory);
+
+        foreach (Match match in matches)
+        {
+            var mulMatch = mulRegex.Match(match.Value);
+            var firstNumber = int.Parse(mulMatch.Groups[1].Value);
+            var secondNumber = int.Parse(mulMatch.Groups[2].Value);
+
+            sum += firstNumber * secondNumber;
+        }
+
+        return sum;
+    }
+
+    public object CalculateSumOfUncorruptedMemoryAccountingForDosAndDonts()
+    {
+        return CalculateSumOfUncorruptedMemory();
+    }
+}
