57e6bd22dcb8b58dc48b2c8f076aac8b0fab6be2
Lars Nyström <nystrom.lasse@gmail.com>
2024-12-05 22:30:28 +0100

    Day05 (#6)
    * Add day05
    
    * Solution for day 5

---
+++ day05/Program.cs
@@ -0,0 +1,129 @@
+﻿using System.Data;
+
+var lines = File.ReadAllLines("input.txt");
+
+var sleighLaunchSafetyManual = new SleighLaunchSafetyManual(lines);
+var sumOfValidUpdates = sleighLaunchSafetyManual.GetSumOfValidUpdates();
+
+System.Console.WriteLine($"Sum of valid updates: {sumOfValidUpdates}");
+
+var sumOfInvalidUpdates = sleighLaunchSafetyManual.GetSumOfInvalidUpdates();
+
+System.Console.WriteLine($"Sum of corrected invalid updates: {sumOfInvalidUpdates}");
+
+public class SleighLaunchSafetyManual
+{
+    private readonly IEnumerable<int[]> rules;
+    private readonly IEnumerable<int[]> updates;
+
+    public SleighLaunchSafetyManual(string[] input)
+    {
+        rules = input.Where(x => x.Contains('|')).Select(x => x.Split('|').Select(x => Convert.ToInt32(x)).ToArray());
+        updates = input.Where(x => x.Contains(',')).Select(x => x.Split(',').Select(x => Convert.ToInt32(x)).ToArray());
+    }
+
+    public IEnumerable<int[]> GetValidUpdates()
+    {
+        var validUpdates = new List<int[]>();
+
+        foreach (var update in updates)
+        {
+            bool valid = true;
+
+            foreach (var rule in rules)
+            {
+                if (update.Contains(rule[0]) && update.Contains(rule[1]))
+                {
+                    // We should test the rule
+                    var updatePair = update.Where(x => x == rule[0] || x == rule[1]);
+                    if (!updatePair.SequenceEqual(rule))
+                    {
+                        valid = false;
+                        break;
+                    }
+                }
+            }
+
+            if (valid) validUpdates.Add(update);
+        }
+
+        return validUpdates;
+    }
+
+    public int GetSumOfValidUpdates()
+    {
+        var validUpdates = GetValidUpdates();
+        return validUpdates.Select(x => x[x.Length / 2]).Sum();
+    }
+
+    public IEnumerable<int[]> GetInvalidUpdates()
+    {
+        var invalidUpdates = new List<int[]>();
+
+        foreach (var update in updates)
+        {
+            bool valid = true;
+
+            foreach (var rule in rules)
+            {
+                if (update.Contains(rule[0]) && update.Contains(rule[1]))
+                {
+                    // We should test the rule
+                    var updatePair = update.Where(x => x == rule[0] || x == rule[1]);
+                    if (!updatePair.SequenceEqual(rule))
+                    {
+                        valid = false;
+                        break;
+                    }
+                }
+            }
+
+            if (!valid) invalidUpdates.Add(update);
+        }
+
+        return invalidUpdates;
+    }
+
+    public int GetSumOfInvalidUpdates()
+    {
+        var invalidUpdates = GetInvalidUpdates();
+        return invalidUpdates.Select(unsorted => unsorted.SortWithRules(rules)).Select(sorted => sorted[sorted.Length / 2]).Sum();
+    }
+}
+
+public static class Extension
+{
+    public static int[] SortWithRules(this int[] array, IEnumerable<int[]> rules)
+    {
+        // Apply some kind of bubble sort (?)
+        // Loop until no rules fails?
+        bool rulesPassed = false;
+        while (!rulesPassed)
+        {
+            rulesPassed = true;
+
+            foreach (var rule in rules)
+            {
+                if (array.Contains(rule[0]) && array.Contains(rule[1]))
+                {
+                    // Both numbers are in
+                    var index0 = Array.FindIndex(array, x => x == rule[0]);
+                    var index1 = Array.FindIndex(array, x => x == rule[1]);
+
+                    if (index0 > index1)
+                    {
+                        // Swap positions
+                        array[index0] = rule[1];
+                        array[index1] = rule[0];
+
+                        // Fail the rule to start over
+                        rulesPassed = false;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return array;
+    }
+}
---
+++ tests/day05tests/UnitTests.cs
@@ -0,0 +1,127 @@
+﻿using Shouldly;
+
+namespace day05tests;
+
+public class UnitTests
+{
+    [Fact]
+    public void Test1()
+    {
+        string[] lines = [
+            "47|53",
+            "97|13",
+            "97|61",
+            "97|47",
+            "75|29",
+            "61|13",
+            "75|53",
+            "29|13",
+            "97|29",
+            "53|29",
+            "61|53",
+            "97|53",
+            "61|29",
+            "47|13",
+            "75|47",
+            "97|75",
+            "47|61",
+            "75|61",
+            "47|29",
+            "75|13",
+            "53|13",
+            "",
+            "75,47,61,53,29",
+            "97,61,53,29,13",
+            "75,29,13",
+            "75,97,47,61,53",
+            "61,13,29",
+            "97,13,75,29,47",
+        ];
+
+        var sleighLaunchSafetyManual = new SleighLaunchSafetyManual(lines);
+        var validUpdates = sleighLaunchSafetyManual.GetValidUpdates().ToArray();
+        validUpdates.Count().ShouldBe(3);
+        validUpdates[0].SequenceEqual([75, 47, 61, 53, 29]).ShouldBeTrue();
+        validUpdates[1].SequenceEqual([97, 61, 53, 29, 13]).ShouldBeTrue();
+        validUpdates[2].SequenceEqual([75, 29, 13]).ShouldBeTrue();
+        sleighLaunchSafetyManual.GetSumOfValidUpdates().ShouldBe(143);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        string[] lines = [
+            "47|53",
+            "97|13",
+            "97|61",
+            "97|47",
+            "75|29",
+            "61|13",
+            "75|53",
+            "29|13",
+            "97|29",
+            "53|29",
+            "61|53",
+            "97|53",
+            "61|29",
+            "47|13",
+            "75|47",
+            "97|75",
+            "47|61",
+            "75|61",
+            "47|29",
+            "75|13",
+            "53|13",
+            "",
+            "75,47,61,53,29",
+            "97,61,53,29,13",
+            "75,29,13",
+            "75,97,47,61,53",
+            "61,13,29",
+            "97,13,75,29,47",
+        ];
+
+        var sleighLaunchSafetyManual = new SleighLaunchSafetyManual(lines);
+        var invalidUpdates = sleighLaunchSafetyManual.GetInvalidUpdates().ToArray();
+        invalidUpdates.Count().ShouldBe(3);
+        invalidUpdates[0].ShouldBe([75, 97, 47, 61, 53]);
+        invalidUpdates[1].ShouldBe([61, 13, 29]);
+        invalidUpdates[2].ShouldBe([97, 13, 75, 29, 47]);
+        sleighLaunchSafetyManual.GetSumOfInvalidUpdates().ShouldBe(123);
+    }
+
+    [Theory]
+    [InlineData(new int[] { 75, 97, 47, 61, 53 }, new int[] { 97, 75, 47, 61, 53 })]
+    [InlineData(new int[] { 61, 13, 29 }, new int[] { 61, 29, 13 })]
+    [InlineData(new int[] { 97, 13, 75, 29, 47 }, new int[] { 97, 75, 47, 29, 13 })]
+    public void ShouldSortWithRules(int[] unsorted, int[] expectedResult)
+    {
+        string[] rulesInput = [
+            "47|53",
+            "97|13",
+            "97|61",
+            "97|47",
+            "75|29",
+            "61|13",
+            "75|53",
+            "29|13",
+            "97|29",
+            "53|29",
+            "61|53",
+            "97|53",
+            "61|29",
+            "47|13",
+            "75|47",
+            "97|75",
+            "47|61",
+            "75|61",
+            "47|29",
+            "75|13",
+            "53|13",
+        ];
+
+        var rules = rulesInput.Select(x => x.Split('|').Select(x => Convert.ToInt32(x)).ToArray());
+        int[] sorted = unsorted.SortWithRules(rules);
+        sorted.ShouldBe(expectedResult);
+    }
+}
