a5ea921f34968652e6db3e1d0a8fcea01ef26507
Lars Nyström <nystrom.lasse@gmail.com>
2024-12-07 21:47:58 +0100

    Day06 (#7)
    * Add day06
    
    * Solution for day 6, part 1
    
    * Messy solution for day 6, part 2

---
+++ tests/day06tests/UnitTests.cs
@@ -0,0 +1,76 @@
+﻿using Shouldly;
+
+namespace day06tests;
+
+public class UnitTests
+{
+    [Fact]
+    public void Test1()
+    {
+        string[] lines = [
+            "....#.....",
+            ".........#",
+            "..........",
+            "..#.......",
+            ".......#..",
+            "..........",
+            ".#..^.....",
+            "........#.",
+            "#.........",
+            "......#...",
+        ];
+        var input = lines.Select(line => line.ToCharArray()).ToArray();
+        var map = new Map(input);
+        map.GuardStartPosition.ShouldBe((4, 6));
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        string[] lines = [
+            "....#.....",
+            ".........#",
+            "..........",
+            "..#.......",
+            ".......#..",
+            "..........",
+            ".#..^.....",
+            "........#.",
+            "#.........",
+            "......#...",
+        ];
+        var input = lines.Select(line => line.ToCharArray()).ToArray();
+        var map = new Map(input);
+        map.GetPositionsCoveredByGuardWalkingOut().ShouldBe(41);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        // The 6 positions with extra obstacles should be
+        // 3,6
+        // 6,7
+        // 7,7
+        // 1,8
+        // 3,8
+        // 7,9
+
+        string[] lines = [
+            "....#.....",
+            ".........#",
+            "..........",
+            "..#.......",
+            ".......#..",
+            "..........",
+            ".#..^.....",
+            "........#.",
+            "#.........",
+            "......#...",
+        ];
+        var input = lines.Select(line => line.ToCharArray()).ToArray();
+        var map = new Map(input);
+        map.CountObstaclePositionsThatMakesTheGuardLoop().ShouldBe(6);
+    }
+
+
+}
