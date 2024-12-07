974c72150f8480aaab8bb90296b2f69ca837449d
Lars Nyström <nystrom.lasse@gmail.com>
2024-12-07 04:09:05 +0100

    Solution for day 6, part 1

--- day06/Program.cs
+++ day06/Program.cs
@@ -1,2 +1,106 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var lines = File.ReadAllLines("input.txt")
+    .Select(l => l.ToCharArray())
+    .ToArray();
+
+var map = new Map(lines);
+var positionsCoveredByGuardWalkingOut = map.GetPositionsCoveredByGuardWalkingOut();
+
+System.Console.WriteLine($"Positions covered by guard walking out: {positionsCoveredByGuardWalkingOut}");
+
+public class Map
+{
+    private readonly char[][] map;
+    private readonly int startX;
+    private readonly int startY;
+
+    public Map(char[][] input)
+    {
+        map = input;
+        startX = -1;
+        startY = 0;
+        bool startFound = false;
+        while (!startFound)
+        {
+            startX = Array.FindIndex(map[startY], c => c.Equals('^'));
+            if (startX >= 0) break;
+            startY++;
+        }
+    }
+
+    public int GetPositionsCoveredByGuardWalkingOut()
+    {
+        bool inside = true;
+        char currentDirection = map[startY][startX];
+        int posX = startX;
+        int posY = startY;
+        HashSet<(int x, int y)> visited = [(posX, posY)];
+
+        while (inside)
+        {
+            // Walk around
+            switch (currentDirection)
+            {
+                case '^':
+                    if (posY - 1 < 0) { inside = false; break; }
+                    if (map[posY - 1][posX].Equals('#'))
+                    {
+                        // Rotate (no move)
+                        currentDirection = '>';
+                    }
+                    else
+                    {
+                        // Move and save
+                        posY--;
+                        visited.Add((posX, posY));
+                    }
+                    break;
+                case '>':
+                    if (posX + 1 >= map[posY].Length) { inside = false; break; }
+                    if (map[posY][posX + 1].Equals('#'))
+                    {
+                        // Rotate (no move)
+                        currentDirection = 'v';
+                    }
+                    else
+                    {
+                        // Move and save
+                        posX++;
+                        visited.Add((posX, posY));
+                    }
+                    break;
+                case 'v':
+                    if (posY + 1 >= map.Length) { inside = false; break; }
+                    if (map[posY + 1][posX].Equals('#'))
+                    {
+                        // Rotate (no move)
+                        currentDirection = '<';
+                    }
+                    else
+                    {
+                        // Move and save
+                        posY++;
+                        visited.Add((posX, posY));
+                    }
+                    break;
+                case '<':
+                    if (posX - 1 < 0) { inside = false; break; }
+                    if (map[posY][posX - 1].Equals('#'))
+                    {
+                        // Rotate (no move)
+                        currentDirection = '^';
+                    }
+                    else
+                    {
+                        // Move and save
+                        posX--;
+                        visited.Add((posX, posY));
+                    }
+                    break;
+            }
+        }
+
+        return visited.Count;
+    }
+
+    public (int x, int y) GuardStartPosition => (startX, startY);
+}
---
+++ tests/day06tests/UnitTests.cs
@@ -0,0 +1,47 @@
+﻿using Shouldly;
+
+namespace day06tests;
+
+public class UnitTests
+{
+    [Fact]
+    public void Test1()
+    {
+        string[] lines = [
+            "....#.....",
+            ".........#",
+            "..........",
+            "..#.......",
+            ".......#..",
+            "..........",
+            ".#..^.....",
+            "........#.",
+            "#.........",
+            "......#...",
+        ];
+        var input = lines.Select(line => line.ToCharArray()).ToArray();
+        var map = new Map(input);
+        map.GuardStartPosition.ShouldBe((4, 6));
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        string[] lines = [
+            "....#.....",
+            ".........#",
+            "..........",
+            "..#.......",
+            ".......#..",
+            "..........",
+            ".#..^.....",
+            "........#.",
+            "#.........",
+            "......#...",
+        ];
+        var input = lines.Select(line => line.ToCharArray()).ToArray();
+        var map = new Map(input);
+        map.GetPositionsCoveredByGuardWalkingOut().ShouldBe(41);
+    }
+
+}
