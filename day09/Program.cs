ccb39358a1c2fa1372a5024f92f42d50a59934e7
Lars Nyström <nystrom.lasse@gmail.com>
2024-12-09 22:59:38 +0100

    Solution for day 9, part 1

--- day09/Program.cs
+++ day09/Program.cs
@@ -1,2 +1,97 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var line = File.ReadAllLines("input.txt")[0];
+
+var drive = new Drive(line);
+drive.Pack();
+var checksum = drive.FilesystemChecksum();
+
+System.Console.WriteLine($"Filesystem checksum: {checksum}");
+
+public class Drive
+{
+    private readonly int[] diskMap;
+    private readonly List<int[]> disk;
+
+    public Drive(string diskMap)
+    {
+        this.diskMap = diskMap.ToArray().Select(x => (int)char.GetNumericValue(x)).ToArray();
+
+        disk = Populate();
+    }
+
+    private List<int[]> Populate()
+    {
+        bool isFile = true;
+        int fileId = 0;
+        var disk = new List<int[]>();
+        foreach (var diskMapInfo in diskMap)
+        {
+            if (isFile)
+            {
+                var file = new int[diskMapInfo];
+                Array.Fill(file, fileId);
+                disk.Add(file);
+                fileId++;
+            }
+            else
+            {
+                var emptySpace = new int[diskMapInfo];
+                Array.Fill(emptySpace, -1);
+                disk.Add(emptySpace);
+            }
+            isFile = !isFile;
+        }
+        return disk;
+    }
+
+    public void Pack()
+    {
+        // First empty space on disk
+        var firstEmptySpace = disk.First(x => x.Contains(-1));
+        var firstEmptySpaceBlock = Array.FindIndex(firstEmptySpace, x => x.Equals(-1));
+
+        // Last block of a file on disk
+        var lastFile = disk.Where(x => x.Any(y => y > 0)).Last();
+        var lastFileBlock = Array.FindLastIndex(lastFile, x => !x.Equals(-1));
+
+        while (true)
+        {
+            // Move block
+            firstEmptySpace[firstEmptySpaceBlock] = lastFile[lastFileBlock];
+            lastFile[lastFileBlock] = -1;
+
+            // First empty space on disk
+            firstEmptySpace = disk.First(x => x.Contains(-1));
+            firstEmptySpaceBlock = Array.FindIndex(firstEmptySpace, x => x.Equals(-1));
+
+            // Last block of a file on disk
+            lastFile = disk.Where(x => x.Any(y => y > 0)).Last();
+            lastFileBlock = Array.FindLastIndex(lastFile, x => !x.Equals(-1));
+
+            var firstEmptySpaceIndex = disk.IndexOf(firstEmptySpace);
+            var lastFileIndex = disk.IndexOf(lastFile);
+
+            if (firstEmptySpaceIndex == lastFileIndex && firstEmptySpaceBlock > lastFileBlock) break;
+            if (firstEmptySpaceIndex > lastFileIndex) break;
+        }
+    }
+
+    public long FilesystemChecksum()
+    {
+        long filesystemChecksum = 0;
+        int position = 0;
+        foreach (var space in disk)
+        {
+            for (int i = 0; i < space.Length; i++)
+            {
+                if (space[i] >= 0)
+                {
+                    filesystemChecksum += space[i] * position;
+                }
+                position++;
+            }
+        }
+        return filesystemChecksum;
+    }
+
+    public List<int[]> Disk => disk;
+}
---
+++ tests/day09tests/UnitTests.cs
@@ -0,0 +1,27 @@
+﻿using Shouldly;
+
+namespace day09tests;
+
+public class UnitTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var drive = new Drive("12345");
+        drive.Disk.Count.ShouldBe(5);
+        drive.Pack();
+        drive.Disk[0].ShouldBe([0]);
+        drive.Disk[1].ShouldBe([2, 2]);
+        drive.Disk[2].ShouldBe([1, 1, 1]);
+        drive.Disk[3].ShouldBe([2, 2, 2, -1]);
+        drive.Disk[4].ShouldBe([-1, -1, -1, -1, -1]);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var drive = new Drive("2333133121414131402");
+        drive.Pack();
+        drive.FilesystemChecksum().ShouldBe(1928);
+    }
+}
