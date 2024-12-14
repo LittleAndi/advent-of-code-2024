0d965997e2438701858056c1108fcb1dbb051aa0
Lars Nyström <nystrom.lasse@gmail.com>
2024-12-14 15:02:52 +0100

    Solution for day 10, part 2

--- day10/Program.cs
+++ day10/Program.cs
@@ -8,6 +8,10 @@ var trailheadScores = map.CalculateTrailheadScores();
 var sumOfTrailheadScores = trailheadScores.Sum();
 System.Console.WriteLine($"Sum of trailhead scores: {sumOfTrailheadScores}");
 
+var trailheadRatings = map.CalculateTrailheadRatings();
+var sumOfTrailheadRatings = trailheadRatings.Sum();
+System.Console.WriteLine($"Sum of trainhead ratings: {sumOfTrailheadRatings}");
+
 public class TopographicMap(int[][] input)
 {
     private readonly int[][] map = input;
@@ -30,6 +34,26 @@ public class TopographicMap(int[][] input)
         }
 
         return trailheadScores;
+    }
+
+    public List<int> CalculateTrailheadRatings()
+    {
+        List<int> trailheadRatings = [];
+
+        for (int y = 0; y < map.Length; y++)
+        {
+            var trailheadIndex = Array.FindIndex(map[y], x => x == 0);
+            while (trailheadIndex >= 0)
+            {
+                // Depth-First Search from this start position, search for nines
+                var score = DepthFirstSearchAllTracks(new Position { X = trailheadIndex, Y = y });
+                trailheadRatings.Add(score);
+
+                trailheadIndex = Array.FindIndex(map[y], trailheadIndex + 1, x => x == 0);
+            }
+        }
+
+        return trailheadRatings;
     }
 
     private int DepthFirstSearch(Position trailhead)
@@ -73,6 +97,50 @@ public class TopographicMap(int[][] input)
                 {
                     stack.Push(new Position { X = position.X - 1, Y = position.Y });
                 }
+            }
+        }
+
+        return score;
+    }
+
+    private int DepthFirstSearchAllTracks(Position trailhead)
+    {
+        var score = 0;
+        var stack = new Stack<Position>();
+        stack.Push(trailhead);
+        while (stack.Count > 0)
+        {
+            var position = stack.Pop();
+
+            if (map[position.Y][position.X] == 9)
+            {
+                score++;
+                continue;
+            }
+
+            // Check all directions for next level
+            // Up
+            if (position.Y - 1 >= 0 && map[position.Y - 1][position.X] == map[position.Y][position.X] + 1)
+            {
+                stack.Push(new Position { X = position.X, Y = position.Y - 1 });
+            }
+
+            // Right
+            if (position.X + 1 < map[position.Y].Length && map[position.Y][position.X + 1] == map[position.Y][position.X] + 1)
+            {
+                stack.Push(new Position { X = position.X + 1, Y = position.Y });
+            }
+
+            // Down
+            if (position.Y + 1 < map.Length && map[position.Y + 1][position.X] == map[position.Y][position.X] + 1)
+            {
+                stack.Push(new Position { X = position.X, Y = position.Y + 1 });
+            }
+
+            // Left
+            if (position.X - 1 >= 0 && map[position.Y][position.X - 1] == map[position.Y][position.X] + 1)
+            {
+                stack.Push(new Position { X = position.X - 1, Y = position.Y });
             }
         }
 
