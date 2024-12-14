61c921d3621ff086d4f5da4ccb17e52717e5ab3c
Lars Nyström <nystrom.lasse@gmail.com>
2024-12-14 15:03:34 +0100

    Day10 (#11)
    * Add day10
    
    * Solution for day 10, part 1
    
    * Solution for day 10, part 2

---
+++ day10/Program.cs
@@ -0,0 +1,156 @@
+﻿var lines = File.ReadAllLines("input.txt")
+    .Where(line => !string.IsNullOrWhiteSpace(line))
+    .Select(l => l.ToCharArray().Select(c => (int)char.GetNumericValue(c)).ToArray())
+    .ToArray();
+
+var map = new TopographicMap(lines);
+var trailheadScores = map.CalculateTrailheadScores();
+var sumOfTrailheadScores = trailheadScores.Sum();
+System.Console.WriteLine($"Sum of trailhead scores: {sumOfTrailheadScores}");
+
+var trailheadRatings = map.CalculateTrailheadRatings();
+var sumOfTrailheadRatings = trailheadRatings.Sum();
+System.Console.WriteLine($"Sum of trainhead ratings: {sumOfTrailheadRatings}");
+
+public class TopographicMap(int[][] input)
+{
+    private readonly int[][] map = input;
+
+    public List<int> CalculateTrailheadScores()
+    {
+        List<int> trailheadScores = [];
+
+        for (int y = 0; y < map.Length; y++)
+        {
+            var trailheadIndex = Array.FindIndex(map[y], x => x == 0);
+            while (trailheadIndex >= 0)
+            {
+                // Depth-First Search from this start position, search for nines
+                var score = DepthFirstSearch(new Position { X = trailheadIndex, Y = y });
+                trailheadScores.Add(score);
+
+                trailheadIndex = Array.FindIndex(map[y], trailheadIndex + 1, x => x == 0);
+            }
+        }
+
+        return trailheadScores;
+    }
+
+    public List<int> CalculateTrailheadRatings()
+    {
+        List<int> trailheadRatings = [];
+
+        for (int y = 0; y < map.Length; y++)
+        {
+            var trailheadIndex = Array.FindIndex(map[y], x => x == 0);
+            while (trailheadIndex >= 0)
+            {
+                // Depth-First Search from this start position, search for nines
+                var score = DepthFirstSearchAllTracks(new Position { X = trailheadIndex, Y = y });
+                trailheadRatings.Add(score);
+
+                trailheadIndex = Array.FindIndex(map[y], trailheadIndex + 1, x => x == 0);
+            }
+        }
+
+        return trailheadRatings;
+    }
+
+    private int DepthFirstSearch(Position trailhead)
+    {
+        var score = 0;
+        HashSet<Position> discovered = [];
+        var stack = new Stack<Position>();
+        stack.Push(trailhead);
+        while (stack.Count > 0)
+        {
+            var position = stack.Pop();
+            if (discovered.Add(position))
+            {
+                if (map[position.Y][position.X] == 9)
+                {
+                    score++;
+                    continue;
+                }
+
+                // Check all directions for next level
+                // Up
+                if (position.Y - 1 >= 0 && map[position.Y - 1][position.X] == map[position.Y][position.X] + 1)
+                {
+                    stack.Push(new Position { X = position.X, Y = position.Y - 1 });
+                }
+
+                // Right
+                if (position.X + 1 < map[position.Y].Length && map[position.Y][position.X + 1] == map[position.Y][position.X] + 1)
+                {
+                    stack.Push(new Position { X = position.X + 1, Y = position.Y });
+                }
+
+                // Down
+                if (position.Y + 1 < map.Length && map[position.Y + 1][position.X] == map[position.Y][position.X] + 1)
+                {
+                    stack.Push(new Position { X = position.X, Y = position.Y + 1 });
+                }
+
+                // Left
+                if (position.X - 1 >= 0 && map[position.Y][position.X - 1] == map[position.Y][position.X] + 1)
+                {
+                    stack.Push(new Position { X = position.X - 1, Y = position.Y });
+                }
+            }
+        }
+
+        return score;
+    }
+
+    private int DepthFirstSearchAllTracks(Position trailhead)
+    {
+        var score = 0;
+        var stack = new Stack<Position>();
+        stack.Push(trailhead);
+        while (stack.Count > 0)
+        {
+            var position = stack.Pop();
+
+            if (map[position.Y][position.X] == 9)
+            {
+                score++;
+                continue;
+            }
+
+            // Check all directions for next level
+            // Up
+            if (position.Y - 1 >= 0 && map[position.Y - 1][position.X] == map[position.Y][position.X] + 1)
+            {
+                stack.Push(new Position { X = position.X, Y = position.Y - 1 });
+            }
+
+            // Right
+            if (position.X + 1 < map[position.Y].Length && map[position.Y][position.X + 1] == map[position.Y][position.X] + 1)
+            {
+                stack.Push(new Position { X = position.X + 1, Y = position.Y });
+            }
+
+            // Down
+            if (position.Y + 1 < map.Length && map[position.Y + 1][position.X] == map[position.Y][position.X] + 1)
+            {
+                stack.Push(new Position { X = position.X, Y = position.Y + 1 });
+            }
+
+            // Left
+            if (position.X - 1 >= 0 && map[position.Y][position.X - 1] == map[position.Y][position.X] + 1)
+            {
+                stack.Push(new Position { X = position.X - 1, Y = position.Y });
+            }
+        }
+
+        return score;
+    }
+
+    struct Position
+    {
+        public int X;
+        public int Y;
+    }
+}
+
---
+++ tests/day10tests/UnitTests.cs
@@ -0,0 +1,126 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+using Shouldly;
+
+namespace day10tests;
+
+public class UnitTests
+{
+    [Fact]
+    public void Test1()
+    {
+        string[] lines = [
+            "...0...",
+            "...1...",
+            "...2...",
+            "6543456",
+            "7.....7",
+            "8.....8",
+            "9.....9",
+        ];
+        var input = lines.Select(l => l.ToCharArray().Select(c => (int)char.GetNumericValue(c)).ToArray()).ToArray();
+        var topographicMap = new TopographicMap(input);
+        var trailheadScores = topographicMap.CalculateTrailheadScores();
+        trailheadScores.Count.ShouldBe(1);
+        trailheadScores.Sum().ShouldBe(2);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        string[] lines = [
+            "89010123",
+            "78121874",
+            "87430965",
+            "96549874",
+            "45678903",
+            "32019012",
+            "01329801",
+            "10456732",
+        ];
+        var input = lines.Select(l => l.ToCharArray().Select(c => (int)char.GetNumericValue(c)).ToArray()).ToArray();
+        var topographicMap = new TopographicMap(input);
+        var trailheadScores = topographicMap.CalculateTrailheadScores();
+        trailheadScores.Count.ShouldBe(9);
+        trailheadScores.Sum().ShouldBe(36);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        string[] lines = [
+            ".....0.",
+            "..4321.",
+            "..5..2.",
+            "..6543.",
+            "..7..4.",
+            "..8765.",
+            "..9....",
+        ];
+
+        var input = lines.Select(l => l.ToCharArray().Select(c => (int)char.GetNumericValue(c)).ToArray()).ToArray();
+        var topographicMap = new TopographicMap(input);
+        var trailheadRatings = topographicMap.CalculateTrailheadRatings();
+        trailheadRatings.Count.ShouldBe(1);
+        trailheadRatings.Sum().ShouldBe(3);
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        string[] lines = [
+            "..90..9",
+            "...1.98",
+            "...2..7",
+            "6543456",
+            "765.987",
+            "876....",
+            "987....",
+        ];
+
+        var input = lines.Select(l => l.ToCharArray().Select(c => (int)char.GetNumericValue(c)).ToArray()).ToArray();
+        var topographicMap = new TopographicMap(input);
+        var trailheadRatings = topographicMap.CalculateTrailheadRatings();
+        trailheadRatings.Count.ShouldBe(1);
+        trailheadRatings.Sum().ShouldBe(13);
+    }
+
+    [Fact]
+    public void Test5()
+    {
+        string[] lines = [
+            "012345",
+            "123456",
+            "234567",
+            "345678",
+            "4.6789",
+            "56789.",
+        ];
+
+        var input = lines.Select(l => l.ToCharArray().Select(c => (int)char.GetNumericValue(c)).ToArray()).ToArray();
+        var topographicMap = new TopographicMap(input);
+        var trailheadRatings = topographicMap.CalculateTrailheadRatings();
+        trailheadRatings.Count.ShouldBe(1);
+        trailheadRatings.Sum().ShouldBe(227);
+    }
+
+    [Fact]
+    public void Test6()
+    {
+        string[] lines = [
+            "89010123",
+            "78121874",
+            "87430965",
+            "96549874",
+            "45678903",
+            "32019012",
+            "01329801",
+            "10456732",
+        ];
+
+        var input = lines.Select(l => l.ToCharArray().Select(c => (int)char.GetNumericValue(c)).ToArray()).ToArray();
+        var topographicMap = new TopographicMap(input);
+        var trailheadRatings = topographicMap.CalculateTrailheadRatings();
+        trailheadRatings.Count.ShouldBe(9);
+        trailheadRatings.Sum().ShouldBe(81);
+    }
+}
